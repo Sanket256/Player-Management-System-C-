54eebec9aeaec1c536b777c5d72fbb80fcd56bd5
Sanket-Devray <89296221+Sanket256@users.noreply.github.com>
2024-03-23 23:26:52 +0530

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,63 +1,76 @@
-#include"player.h"
-#include<iostream>
-#include<string.h>
-#include<iomanip>
-using namespace std;
-	Player::Player(){
-        this->jerSeyNo=0;
-        strcpy(this->name,"Name not given");
-        this->runs=0;
-        this->wickets=0;
-        this->matches=0;
-    }
-	Player::Player(int v1,char* v2,int v3,int v4,int v5){
-        this->jerSeyNo= v1;
-        strcpy(this->name,v2);
-        this->runs=v3;
-        this->wickets=v4;
-        this->matches=v5;
-    }
+#include "player.h"
+#include <iomanip>
 
-	int Player::getJno(){
-        return this->jerSeyNo;
-    }
-	char* Player::getName(){
-        return this->name;
-    }
-	int Player::getRun(){
-        return this->runs;
-    }
-	int Player::getWicket(){
-        return this->wickets;
-    }
-	int Player::getMatches(){
-        return this->matches;
-    }
-
-	void Player::setJno(int v1){
-         this->jerSeyNo= v1;
-    }
-	void Player::setName(char* v2){
-        strcpy(this->name,v2);
-    }
-	void Player::setRun(int v3){
-         this->runs=v3;
-    }
-	void Player::setWicket(int v4){
-        this->wickets=v4;
-    }
-	void Player::setMatches(int v5){
-        this->matches=v5;
-    }
-
-	void Player::displayPlayer() {
-    std::cout << "+_______________________________________+\n"
-                 "|            PLAYER INFORMATION         |\n"
-                 "+_______________________________________+\n"
-                 "| Jersey No        | " << jerSeyNo << "\n"
-                 "| Name             | " << name << "\n"
-                 "| Runs             | " << runs << "\n"
-                 "| Matches Played   | " << matches << "\n"
-                 "| Wickets          | " << wickets << "\n"
-                 "+_______________________________________+\n";
+Player::Player()
+{
+	j_no=0;
+	strcpy(name,"Not given");
+	runs=0;
+	matches=0;
+	wickets=0;
+}
+/////////////////////////////////////////////
+Player::Player(int j ,char* nm, int r,int m, int w)
+{
+	j_no=j;
+	strcpy(name,nm);
+	runs=r;
+	matches=m;
+	wickets=w;
+}
+//////////////////////////////////////////////////
+void Player::setJ_no(int j)
+{
+	j_no=j;
+}
+////////////////////////////////////////////
+void Player::setName(char* nm)
+{
+	strcpy(name,"nm");
+}
+////////////////////////////////////////////
+void Player::setRuns(int r)
+{
+	runs=r;
+}
+///////////////////////////////////////
+void Player::setMatches(int m)
+{
+	matches=m;
+}
+///////////////////////////////////////////
+void Player::setWicket(int w)
+{
+	wickets=w;
+}
+//////////////////////////////////////////
+int Player::getJ_no()
+{
+	return j_no;
+}
+///////////////////////////////////////////////
+char* Player::getName()
+{
+	return name;
+}
+////////////////////////////////////////////
+int Player::getRuns()
+{
+	return runs;
+}
+///////////////////////////////////////////
+int Player::getMatches()
+{
+	return matches;
+}
+//////////////////////////////////////
+int Player::getWickets()
+{
+	return wickets;
+}
+//////////////////////////////////////////
+void Player::display()
+{ 
+	cout<<"|"<<setw(14)<<j_no<<"|"<<setw(14)<<name<<"|"<<setw(14)<<runs<<"|"
+			<<setw(16)<<matches<<"|"<<setw(14)<<wickets<<"|"<<"\n";
 }
