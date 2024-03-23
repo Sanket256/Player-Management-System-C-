ba2d7392f9ed105cf5c815d8176936b7a19351fb
Sanket-Devray <89296221+Sanket256@users.noreply.github.com>
2024-03-23 23:24:07 +0530

    Update linkedlist.h
    same updated

--- linkedlist.h
+++ linkedlist.h
@@ -1,19 +1,22 @@
-#include"node.h"
-class LinkedList{
-
-    Node* start;
-
-    public:
-
-    LinkedList();
-    // void insertBeg(Node* );
-	void addPlayerToPosition(Player&,int);
-	void DeletePalyerFromPosition(int pos);	
-	void displayInfo();
-	bool searchPlayer(int);
-	bool updatePlayer(int);
-	void topThreePlayers();
-    void readDataFromFile(const char* );
-    void writeDataToFile(const char* );
-	~LinkedList();
+#include"node.cpp"
+//template<class T>
+#include"fstream"
+class LinkedList
+{
+	 Node<Player> *start;
+	 
+	 public:
+	 	LinkedList();
+	 	Node<Player>* getStart();
+	 	void insertBeg(Player );
+	 	void insertPos(Player,int);
+	 	void deletePos(int);
+	 	Node<Player>* searchPlayer(int);
+	 	bool updateRuns(int ,int);
+	 	bool updateWickets(int , int);
+	 	bool updateMatches(int, int);
+	 	void top3PlayersByRuns();
+	 	void display();
+		void writeDataToFile(const char* );
+		void readDataFromFile(const char* );
 };
