aa35880749c8c05f4ea3bd0ae59f35ae451f1114
Sanket-Devray <89296221+Sanket256@users.noreply.github.com>
2024-03-23 23:25:15 +0530

    Update main.cpp
    resuced some cases beacuse they dont have meanig according to me and aslo have curresponding methods for the same

--- main.cpp
+++ main.cpp
@@ -1,114 +1,144 @@
-#include "LinkedList.cpp"
-#include <fstream>
+#include"linklist.h"
 using namespace std;
 
-int main()
-{
-    LinkedList lt;
-    int choice = 0;
-    ofstream hout("players.txt", ios::app);  
-    ifstream hin("players.txt"); 
+int main() {
+	LinkedList lt;
+	int choice=0;
+	
+	lt.readDataFromFile("player.txt");
 
-    do {
-        cout << "+______________________________________+\n"
-             << "|           MENU OPTIONS               |\n"
-             << "+______________________________________+\n"
-             << "| 1) Add Player                        |\n"
-             << "| 2) Search by jersey no               |\n"
-             << "| 4) Delete Player                     |\n"
-             << "| 5) Update Player runs                |\n"
-             << "| 6) Update Player wickets             |\n"
-             << "| 7) Update Player matches             |\n"
-             << "| 8) Top 3 on runs                     |\n"
-             << "| 9) Top 3 on wickets                  |\n"
-             << "|10) Display Player                    |\n"
-             << "|11) Exit                              |\n"
-             << "+______________________________________+\n"
-             << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice) {
-            case 1: {
-                int JerseyNo;
-                cout << "\nEnter the jersey no: ";
-                cin >> JerseyNo;
-
-                char name[20];
-                cout << "\nEnter the name: ";
-                cin >> name;
-
-                int run;
-                cout << "\nEnter the run: ";
-                cin >> run;
-
-                int wicket;
-                cout << "\nEnter the wicket: ";
-                cin >> wicket;
-
-                int matches;
-                cout << "\nEnter the matches: ";
-                cin >> matches;
-
-                int pos;
-                cout << "\nEnter the position to add player: ";
-                cin >> pos;
-
-                Player p1(JerseyNo, name, run, wicket, matches);
-                lt.addPlayerToPosition(p1, pos);
-
-                break;
-            }
-            case 2: {
-                int pos;
-                cout << "\nEnter the position to delete player: ";
-                cin >> pos;
-                lt.DeletePalyerFromPosition(pos);
-                break;
-            }
-            case 3: 
-                lt.displayInfo();
-                break;
-            case 4: {
-                int jno;
-                cout << "\nEnter the jersey no to search player: ";
-                cin >> jno;
-                if (lt.searchPlayer(jno))
-                    cout << "\nFound !!!";
-                else
-                    cout << "\nNot found !!!";
-                break;
-            }
-            case 5: {
-                int jno;
-                cout << "\nEnter the jersey no to update player: ";
-                cin >> jno;
-                if (lt.updatePlayer(jno))
-                    cout << "\nUpdated Successfully !!!";
-                else
-                    cout << "\nNot found !!!";
-                break;
-            }
-            case 6: {
-                lt.topThreePlayers();
-                break;
-            }
-            case 7: {
-                if (!hin.is_open()) {
-                    cout << "Error opening file." << endl;
-                    return 1;
-                }
-                lt.readDataFromFile("players.txt");
-                hin.close();
-                break;
-            }
-            case 8: 
-                cout << "\nExit Program ";
-                return 0;
-            default:
-                cout << "\n\nInvalid choice ";
-                break;
-        }
-    } while (choice != 8);
-
-    return 0;
+	while(choice != 9)
+	{
+	     cout << "\n+--------------------------------------+\n"
+                 "|           MENU OPTIONS               |\n"
+                 "+--------------------------------------+\n"
+                 "| 1) Add Player                        |\n"
+                 "| 2) Search by jersey no               |\n"
+                 "| 3) Delete Player                     |\n"
+                 "| 4) Update Player runs                |\n"
+                 "| 5) Update Player wickets             |\n"
+                 "| 6) Update Player matches             |\n"
+                 "| 7) Top 3 on runs                     |\n"
+                 "| 8) Display Player                     |\n"
+                 "| 9) Exit                               |\n"
+                 "+--------------------------------------+\n"
+                 "Enter your choice: ";
+		cin>>choice;
+			
+		switch(choice)
+		{
+			case 1:{
+				int j_no,runs,wickets,matches,pos;
+				char name[20];
+				cout<<"\nEnter jersey no :";
+				cin>>j_no;
+				cout<<"\nEnter name :";
+				cin>>name;
+				cout<<"\nEnter runs :";
+				cin>>runs;
+				cout<<"\nEnter wickets :";
+				cin>>wickets;
+				cout<<"\nEnter matches :";
+				cin>>matches;
+				cout<<"\nEnter Posotion :";
+				cin>>pos;
+				
+				Player p1(j_no,name,runs,wickets,matches);
+				lt.insertPos(p1,pos);
+				break;
+			}
+			case 2:{
+				int j_no;
+				cout<<"\nEnter jersey no to search :";
+				cin>>j_no;
+				if(Node<Player> *p = lt.searchPlayer(j_no))
+				{
+					cout<<"\nPlayer is found...\n\n";
+					p->getData().display();
+				
+				}
+				else
+				{
+					cout<<"\nPlayer is not found\n";
+				}
+				break;
+			}
+			case 3:{
+				int pos;
+				cout<<"\nEnter position of player to delete :";
+				cin>>pos;
+				lt.deletePos(pos);
+				break;
+			}
+			
+			case 4:{
+				int j_no,runs;
+				cout<<"\nEnter jersey no to update runs :";
+				cin>>j_no;
+				cout<<"\nEnter new value of runs :";
+				cin>>runs;
+				if(lt.updateRuns(j_no,runs))
+				{
+					cout<<"\nPlayer runs updated successfully";
+				}
+				else
+				{
+					cout<<"\nPlayer not found";
+				}
+				break;
+			}
+			case 5:{
+				int j_no,wicket;
+				cout<<"\nEnter jersey no to update wickets :";
+				cin>>j_no;
+				cout<<"\nEnter new value of wickets :";
+				cin>>wicket;
+				if(lt.updateWickets(j_no,wicket))
+				{
+					cout<<"\nPlayer wickets updated successfully";
+				}
+				else
+				{
+					cout<<"\nPlayer not found";
+				}
+				break;
+			}
+			case 6:{
+				int j_no, match;
+				cout<<"\nEnter jersey no to update matches :";
+				cin>>j_no;
+				cout<<"\nEnter new value of matches :";
+				cin>>match;
+				if(lt.updateMatches(j_no,match))
+				{
+					cout<<"\nPlayer matches updated successfully";
+				}
+				else
+				{
+					cout<<"\nPlayer not found";
+				}
+				break;
+			}
+			case 7:{
+				lt.top3PlayersByRuns();
+				break;
+			}
+			case 8:{
+				lt.display();
+				break;
+			}	
+			case 9:{
+				cout<<"\nExiting....";
+				break;
+			}
+			default:{
+				
+				break;
+			}
+		}
+	}	
+	
+	lt.writeDataToFile("player.txt");
+	return 0;
+	
 }
